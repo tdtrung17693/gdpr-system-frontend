8b38ed7836c24c574c2597a44aa42a88bf19a828
Dao Duy Long <long.dao@netpower.lan>
2020-09-05 11:56:51 +0700

    merge done

--- src/pages/Servers/Components/CreateServerModal/CreateServerModal.tsx
+++ src/pages/Servers/Components/CreateServerModal/CreateServerModal.tsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { Modal, Button } from 'antd';
+import { Modal, Button, Input, Form, DatePicker } from 'antd';
 
 export default class CreateServerModal extends Component {
+  constructor(props: any) {
+    super(props);
+  }
+  //modal
   state = {
     loading: false,
     visible: false,
@@ -24,8 +28,32 @@ export default class CreateServerModal extends Component {
     this.setState({ visible: false });
   };
 
+  //form
+
+  FormItem = Form.Item;
+
+  layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  validateMessages = {
+    required: '${label} is required!',
+    types: {
+      email: '${label} is not validate email!',
+      number: '${label} is not a validate number!',
+    },
+    number: {
+      range: '${label} must be between ${min} and ${max}',
+    },
+  };
+
+  onFinish = (values: any) => {
+    console.log(values);
+  };
   render() {
     const { visible, loading } = this.state;
+
     return (
       <>
         <Button type="primary" onClick={this.showModal}>
@@ -37,7 +65,12 @@ export default class CreateServerModal extends Component {
           onOk={this.handleOk}
           onCancel={this.handleCancel}
           footer={[
-            <Button key="submit" type="primary" loading={loading} onClick={this.handleOk}>
+            <Form.Item wrapperCol={{ ...this.layout.wrapperCol, offset: 8 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>,
+            <Button key="submit" htmlType="submit" type="primary" loading={loading} onClick={this.handleOk}>
               Save
             </Button>,
             <Button key="back" onClick={this.handleCancel}>
@@ -45,11 +78,24 @@ export default class CreateServerModal extends Component {
             </Button>,
           ]}
         >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
+          <Form {...this.layout} name="nest-messages" onFinish={this.onFinish} validateMessages={this.validateMessages}>
+            <Form.Item name={['user', 'name']} label="Server Name" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item name={['user', 'ipaddress']} label="IpAddress" rules={[{ type: 'email' }, { required: true }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item name={['user', 'startdate']} label="StartDate" rules={[{ required: true }]}>
+              <Input.Group compact>
+                <DatePicker style={{ width: '100%' }} />
+              </Input.Group>
+            </Form.Item>
+            <Form.Item name={['user', 'enddate']} label="EndDate" rules={[{ required: true }]}>
+              <Input.Group compact>
+                <DatePicker style={{ width: '100%' }} />
+              </Input.Group>
+            </Form.Item>
+          </Form>
         </Modal>
       </>
     );
